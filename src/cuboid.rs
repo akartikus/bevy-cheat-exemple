c826874412df652ab5398f09d2188c7d99c95150
akartikus <sitraka.razafimamy@gmail.com>
2024-02-23 21:49:10 +0100

    Clean cuboid to plugin, add velocity and basic movement

---
+++ src/cuboid.rs
@@ -0,0 +1,37 @@
+use bevy::prelude::*;
+
+use crate::movement::Velocity;
+
+const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 0.1);
+
+#[derive(Bundle)]
+struct CuboidBundle {
+    velocity: Velocity,
+    model: PbrBundle,
+}
+
+pub struct CuboidPlugin;
+
+impl Plugin for CuboidPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_cuboid);
+    }
+}
+
+fn spawn_cuboid(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(CuboidBundle {
+        velocity: Velocity {
+            value: STARTING_VELOCITY,
+        },
+        model: PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::rgb_u8(124, 144, 255)),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+mod cuboid;
+mod movement;
+
 use bevy::prelude::*;
+use cuboid::CuboidPlugin;
+use movement::MovementPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(CuboidPlugin)
+        .add_plugins(MovementPlugin)
         .add_systems(Startup, setup)
         .run();
 }
@@ -19,14 +26,6 @@ fn setup(
         ..default()
     });
 
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::rgb_u8(124, 144, 255)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -39,7 +38,7 @@ fn setup(
 
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Z),
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
