0261ad72fa7095367531c581fd929cb65be2b92e
akartikus <sitraka.razafimamy@gmail.com>
2024-03-17 20:04:56 +0100

    #4 Add physics

--- src/cuboid.rs
+++ src/cuboid.rs
@@ -1,6 +1,6 @@
-use bevy::prelude::*;
-
 use crate::{movement::Velocity, Player};
+use bevy::prelude::*;
+use bevy_xpbd_3d::{components::RigidBody, plugins::collision::Collider};
 
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, -1.0);
 
@@ -8,6 +8,8 @@ const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, -1.0);
 struct CuboidBundle {
     velocity: Velocity,
     model: PbrBundle,
+    rigid_body: RigidBody,
+    collider: Collider,
     player: Player,
 }
 
@@ -34,6 +36,8 @@ fn spawn_cuboid(
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
         },
+        rigid_body: RigidBody::Dynamic,
+        collider: Collider::cuboid(1.0, 1.0, 1.5),
         player: Player,
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ mod cuboid;
 mod movement;
 
 use bevy::prelude::*;
+use bevy_xpbd_3d::{
+    components::RigidBody,
+    plugins::{collision::Collider, PhysicsPlugins},
+};
 use camera::CameraPlugin;
 use cuboid::CuboidPlugin;
 use movement::MovementPlugin;
@@ -16,6 +20,7 @@ fn main() {
         .add_plugins(CuboidPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(CameraPlugin)
+        .add_plugins(PhysicsPlugins::default())
         .add_systems(Startup, setup)
         .run();
 }
@@ -25,12 +30,28 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // circular base
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Circle::new(4.0)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-        ..default()
-    });
+    // commands.spawn((
+    //     RigidBody::Static,
+    //     Collider::cylinder(0.02, 4.0),
+    //     PbrBundle {
+    //         mesh: meshes.add(Circle::new(4.0)),
+    //         material: materials.add(Color::WHITE),
+    //         transform: Transform::from_rotation(Quat::from_rotation_x(
+    //             -std::f32::consts::FRAC_PI_2,
+    //         )),
+    //         ..default()
+    //     },
+    // ));
+    // Plane
+    commands.spawn((
+        RigidBody::Static,
+        Collider::cuboid(8.0, 0.002, 8.0),
+        PbrBundle {
+            mesh: meshes.add(Plane3d::default().mesh().size(8.0, 8.0)),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
+            ..default()
+        },
+    ));
 
     // light
     commands.spawn(PointLightBundle {
